38c64027c0baeda3db855a8b3120c1ebb9acb52f
Hai Vu <55154371+Haidvu@users.noreply.github.com>
2021-12-09 12:09:48 -0600

    Updates

---
+++ precache-manifest.06474e47fa22775e33d728c6a6283657.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f119a883491d104600d7fc4130896b",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "9c7a022a16e94fcef8cf",
+    "url": "/Dashboard/static/css/2.bd66a91b.chunk.css"
+  },
+  {
+    "revision": "fcc7b468d717eaed7c88",
+    "url": "/Dashboard/static/css/main.60ff42bb.chunk.css"
+  },
+  {
+    "revision": "9c7a022a16e94fcef8cf",
+    "url": "/Dashboard/static/js/2.1609ca5c.chunk.js"
+  },
+  {
+    "revision": "71d4ec38a5ca7e2b8b5a4cd1cfca82a9",
+    "url": "/Dashboard/static/js/2.1609ca5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759de3c285d50baafd5d",
+    "url": "/Dashboard/static/js/3.086b3f6b.chunk.js"
+  },
+  {
+    "revision": "fcc7b468d717eaed7c88",
+    "url": "/Dashboard/static/js/main.0a235901.chunk.js"
+  },
+  {
+    "revision": "1571f981184429c91c942e436f145d3c",
+    "url": "/Dashboard/static/js/mapbox-gl-csp-worker.ebb076d9.worker.js"
+  },
+  {
+    "revision": "05718d533d12e938865c",
+    "url": "/Dashboard/static/js/runtime-main.38314a4d.js"
+  },
+  {
+    "revision": "d8af621ca3358a819610d8a4b434da07",
+    "url": "/Dashboard/static/media/ozone.d8af621c.jpg"
+  },
+  {
+    "revision": "edb007b73ec455679084eefbb2c1e04a",
+    "url": "/Dashboard/static/media/ozonehealthchest.edb007b7.jpg"
+  },
+  {
+    "revision": "f626d62d589f88341da83b873032f5f6",
+    "url": "/Dashboard/static/media/refinery.f626d62d.jpeg"
+  }
+]);
